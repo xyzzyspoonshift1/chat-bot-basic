b337d04299942ad7c753addd8be84b211e47b9bc
xyzzyspoonshift1 <78494298+xyzzyspoonshift1@users.noreply.github.com>
2024-11-05 11:03:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def fetch_conversion_factor(source,final):
         return None
 
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     app.run(debug=True)
